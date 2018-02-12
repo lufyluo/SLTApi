1f57a8ffb95b3219addb7185f29bd9232e6a2eed
lufyluo <lufyluo@gmail.com>
2018-02-12 18:04:23 +0800

    1、添加被忽略的文件

---
+++ AppApi/Log/Logger.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+
+namespace AppApi.Log
+{
+    public class Logger
+    {
+        private static ILog _logger;
+        private static readonly object LogLock = 1;
+        private Logger() { }
+
+        public static ILog Log
+        {
+            get
+            {
+
+                if (_logger == null)
+                {
+                    lock (LogLock)
+                    {
+                        if (_logger == null)
+                        {
+                            _logger = LogManager.GetLogger("LogFileAppender");
+                        }
+                    }
+                }
+                return _logger;
+            }
+        }
+
+
+    }
+}
